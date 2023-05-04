8708966316000d102bb734777749bb254a87a103
traxx10 <codetraxx10@gmail.com>
2023-05-04 08:45:55 +0100

    added dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/traxx10/puppy
 
 go 1.20
+
+require github.com/traxx10/dog v0.0.0-20230504074350-52499ca1cacf // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/traxx10/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
